fcb082ebb5bca86de8d59cbf5b10d272dbbf67e8
KumudaBG <kumuda.benakanahalliguruprasadamurt@sjsu.edu>
2021-05-08 20:57:51 -0700

    counter implemented

---
+++ src/main/java/WebRequestCounter.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public interface WebRequestCounter {
+	void increment();
+
+	long getCurrentCount();
+
+	void flush();
+}
---
+++ src/test/java/WebRequestCounterImplTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+public class WebRequestCounterImplTest {
+
+	@Test
+	public void testIncrement_Valid()
+	{
+		
+	}
+}
